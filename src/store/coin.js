a12fd0da4dccecc0c3b50d96ac4e5e8b251404df
Valentin Youshkevich <v.yushkevich@bsl.dev>
2022-08-11 10:03:51 +0300

    view coin template and logick

--- src/store/coin.js
+++ src/store/coin.js
@@ -5,20 +5,22 @@ import { axios } from '@/services/axiosInstance';
 export const useCoinStore = defineStore('coin', {
   state() {
     return {
+      localCoinId: null,
       coinFullInfo: null,
     };
   },
   getters: {
     coinDisplayInfo() {
-      return this.coinFullInfo.DISPLAY.BTC.USD;
+      return this.coinFullInfo?.DISPLAY[this.localCoinId].USD || {};
     },
     coinRawInfo() {
-      return this.coinFullInfo.RAW.BTC.USD;
+      return this.coinFullInfo?.RAW[this.localCoinId].USD || {};
     },
   },
   actions: {
     async getCoinInfo(coinId) {
       try {
+        this.localCoinId = coinId;
         const response = await axios.get(`/pricemultifull?fsyms=${coinId}&tsyms=USD`);
         console.log('response.data', response.data);
         this.coinFullInfo = response.data;
--- src/views/CoinView.vue
+++ src/views/CoinView.vue
@@ -1,16 +1,107 @@
 <template>
-  <div>
-    {{route.params.coinId}}
+  <div class="coinCard">
+    <div class="img__contaner">
+      <img class="card__image" alt="Vue logo" :src="imgPath" />
+    </div>
+    <div class="coin__descr">
+      <h2 class="coin__title">
+        Символ {{ coinStore.coinDisplayInfo.FROMSYMBOL }}
+      </h2>
+      <div class="coin__marketplace">
+        Торговая площадка {{ coinStore.coinDisplayInfo.MARKET }}
+      </div>
+      <div class="coin__price">Цена {{ coinStore.coinDisplayInfo.PRICE }}</div>
+      <div class="coin__change-hour">
+        Колебание цены час
+        <span :class="hourPriceClasses">{{
+          toFixedNum(coinStore.coinRawInfo.CHANGEHOUR, 2)
+        }}</span>
+      </div>
+      <div class="coin__change-day">
+        Колебание цены сутки
+        <span :class="dayPriceClasses">
+          {{ toFixedNum(coinStore.coinRawInfo.CHANGE24HOUR, 2) }}
+        </span>
+      </div>
+    </div>
+    <button type="button" class="returnButton" @click="returnBack">
+      Назад
+    </button>
   </div>
 </template>
 
 <script setup>
-import { useRoute } from 'vue-router';
+import { computed } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+import { useCoinStore } from '@/store/coin';
 
 const route = useRoute();
+const coinStore = useCoinStore();
+coinStore.getCoinInfo(route.params.coinId);
 
+const imgPath = computed(
+  () => `https://www.cryptocompare.com/${coinStore.coinDisplayInfo.IMAGEURL}`,
+);
+
+const hourPriceClasses = computed(() => ({
+  goodPrice: coinStore.coinRawInfo.CHANGEHOUR > 0,
+  badPrice: coinStore.coinRawInfo.CHANGEHOUR < 0,
+}));
+
+const dayPriceClasses = computed(() => ({
+  goodPrice: coinStore.coinRawInfo.CHANGE24HOUR > 0,
+  badPrice: coinStore.coinRawInfo.CHANGE24HOUR < 0,
+}));
+
+function toFixedNum(num, fixedParam) {
+  if (num) return num.toFixed(fixedParam);
+  return 0;
+}
+
+const router = useRouter();
+
+function returnBack() {
+  router.push('/');
+}
 </script>
 
 <style lang="scss" scoped>
-
+.coinCard {
+  margin: 0px auto;
+  border: 1px solid #ccc;
+  border-radius: 15px;
+  max-width: 1300px;
+  display: flex;
+  align-items: center;
+  position: relative;
+   padding-bottom: 40px;
+}
+.coin__descr {
+  padding: 30px;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+}
+.coin__price,
+.coin__change-hour,
+.coin__change-day {
+  margin-top: 20px;
+}
+.goodPrice {
+  color: rgb(22, 214, 22);
+}
+.badPrice {
+  color: red;
+}
+.returnButton {
+  padding: 10px;
+  border: 1px solid blue;
+  border-radius: 15px;
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  outline: none;
+  cursor: pointer;
+}
 </style>
